d279130136ef4e9c96a4a54d397510577d0b0fcf
nicolas <nicolasfelipe3702@gmail.com>
2024-07-04 17:06:55 -0300

    atualização exercicio

--- busca-clientes.php
+++ busca-clientes.php
@@ -1,22 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$dbname = "meubanco";
+    require_once("conexao.php");
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM cliente");
-    $stmt->execute();
-
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $json = json_encode($results);
-    print($json);
-
-} catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-
-$conn = null; 
+    try {
+        $stmt = $conn->prepare("SELECT * FROM cliente");
+        $stmt->execute();
+    
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $json = json_encode($results);
+        print($json);
+    
+    } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    
+    $conn = null; 
 ?>
+
 
--- js/busca-clientes.js
+++ js/busca-clientes.js
@@ -1,8 +1,31 @@
-const btn = document.getElementById("btn");
+const btnBusca = document.getElementById("btnBusca");
+const btnIncluirCliente = document.getElementById("btnIncluirCliente");
+const btnIncluir = document.getElementById("btnIncluir");
 const content = document.getElementById("content");
 
+btnIncluirCliente.addEventListener("click", (e) => {
+   const frmIncluirCliente = document.getElementById("frmIncluirCliente");
+   frmIncluirCliente.style.display = "block";
+})
+
+btnIncluir.addEventListener("click", (e) => {
+   e.preventDefault();
+   const xhr = new XMLHttpRequest();
+   let cliente = new FormData(document.getElementById("frmIncluirCliente"));
+   xhr.onload = () => {
+      if (xhr.status == 200) {
+         alert(xhr.responseText);
+         alert("Inclusão OK");
+      } else {
+         alert("Erro inclusão");
+      }
+   }
+   xhr.open("POST", "insert-cliente.php");
+   xhr.send(cliente);
+})
+
 btn.addEventListener("click", buscaClientes());
-document.addEventListener("load", buscaClientes());
+document.addEventListener("DOMContentLoaded", buscaClientes());
 
 function buscaClientes() {
    const req = new XMLHttpRequest();
@@ -11,11 +34,10 @@ function buscaClientes() {
          let html = "<table class='table table-bordered table-hover table-sm'>";
          html += "<tr><th>Cod</th><th>Nome</th><th>Email</th></tr>";
          const vetorClientes = JSON.parse(this.responseText);
-         console.log(vetorClientes);
          // buscar registros de clientes
          for (let cliente of vetorClientes) {
             html += "<tr>";
-            html += `<td>${cliente.codigo}</td>`;
+            html += `<td>${cliente.cod}</td>`;
             html += `<td>${cliente.nome}</td>`;
             html += `<td>${cliente.email}</td>`;
             html += "</tr>";
